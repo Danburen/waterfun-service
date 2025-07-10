7070fa04abb13415261ca3e70cf04bca631aad95
Danburen <2964361672@qq.com>
2025-07-10 21:00:19 +0800

    feat: add user info store logic

--- src/main/java/org/waterwood/waterfunservice/service/authServices/RegisterService.java
+++ src/main/java/org/waterwood/waterfunservice/service/authServices/RegisterService.java
@@ -5,12 +5,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.waterwood.waterfunservice.DTO.common.ResponseCode;
 import org.waterwood.waterfunservice.DTO.common.ApiResponse;
+import org.waterwood.waterfunservice.entity.security.EncryptionDataKey;
 import org.waterwood.waterfunservice.repository.UserDatumRepo;
+import org.waterwood.waterfunservice.service.EncryptedKeyService;
 import org.waterwood.waterfunservice.service.dto.LoginServiceResponse;
 import org.waterwood.waterfunservice.DTO.request.RegisterRequest;
 import org.waterwood.waterfunservice.entity.user.User;
 import org.waterwood.waterfunservice.entity.user.UserDatum;
 import org.waterwood.waterfunservice.repository.UserRepository;
+import org.waterwood.waterfunservice.utils.security.KeyEncryptionHelper;
 import org.waterwood.waterfunservice.utils.security.PasswordUtil;
 import org.waterwood.waterfunservice.utils.streamApi.AuthValidator;
 
@@ -23,6 +26,8 @@ public class RegisterService {
     private UserRepository userRepo;
     @Autowired
     private UserDatumRepo userDatumRepo;
+    @Autowired
+    private EncryptedKeyService encryptedKeyService;
 
     @Transactional
     public ApiResponse<LoginServiceResponse> register(RegisterRequest request, String uuid) {
@@ -41,16 +46,27 @@ public class RegisterService {
                     User user = new User();
                     user.setUsername(request.getUsername());
                     user.setPasswordHash(PasswordUtil.encryptPassword(request.getPassword()));
-                    // Create a new user datum
-                    Long userId = user.getId();
-                    UserDatum userDatum = new UserDatum();
-                    userDatum.setId(userId);
-                    userDatum.setEmail(request.getEmail());
-                    userDatum.setPhone(request.getPhoneNumber());
-                    userDatum.setPhoneVerified(true);
-                    userRepo.save(user);
-                    userDatumRepo.save(userDatum);
-                    return authService.validateTokens(null,null,user);
+
+                    // Random pick dek and encrypted
+                    return encryptedKeyService.randomPickEncryptionKey().map(
+                            key->{
+                                // Create a new user datum
+                                Long userId = user.getId();
+                                UserDatum userDatum = new UserDatum();
+                                userDatum.setId(userId);
+                                userDatum.setEncryptionKeyId(key.getId());
+                                try {
+                                    userDatum.setEmail(KeyEncryptionHelper.encryptField(request.getEmail(),key));
+                                    userDatum.setPhone(KeyEncryptionHelper.encryptField(request.getPhoneNumber(),key));
+                                } catch (Exception e) {
+                                    throw new RuntimeException(e);
+                                }
+                                userDatum.setPhoneVerified(true);
+                                userRepo.save(user);
+                                userDatumRepo.save(userDatum);
+                                return authService.validateTokens(null,null,user);
+                            }
+                    ).orElse(ApiResponse.failure(ResponseCode.INTERNAL_SERVER_ERROR));
                 });
     }
 }
